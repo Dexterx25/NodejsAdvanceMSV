326080f8a0f52cebc4be1a146f4fe591e6c5b017
dexterx25 <davidblancokdf@gmail.com>
2021-10-14 00:19:50 -0500

    testfinished

--- src/MSV_mqtt/components/publisher/network.ts
+++ src/MSV_mqtt/components/publisher/network.ts
@@ -3,7 +3,7 @@ import mqtt from 'mqtt';
 import { config } from '../../../configurations/index';
 const clientId = `mqtt_JuanDavidBlancoVergara`;
 const connectUrl = `mqtt://${config.mqtt_config.host}:${config.mqtt_config.port}`;
-console.log('this is the url--->', connectUrl);
+console.log('MQTT URL--->', connectUrl);
 const pubClient = mqtt.connect(connectUrl, {
   clientId
 });
